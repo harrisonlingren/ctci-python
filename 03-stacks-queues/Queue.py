f46fbd42168784806cea29921fd7e5481b17f69b
Harrison Lingren <harrisonlingren@gmail.com>
2017-08-12 22:24:30 -0700

    fixed Queue issue

--- 03-stacks-queues/Queue.py
+++ 03-stacks-queues/Queue.py
@@ -1,8 +1,12 @@
 class Queue(object):
 
     def __init__(self):
-        self.first = Node(None)
-        self.last = Node(None)
+        self.first = None
+        self.last = None
+        self.size = 0
+
+    def __len__(self):
+        return self.size
 
     def dequeue(self):
         if self.first is None:
@@ -11,6 +15,7 @@ class Queue(object):
         self.first = self.first.next
         if self.first is None:
             self.last = None
+        self.size -= 1
         return old
 
     def enqueue(self, val):
@@ -20,12 +25,13 @@ class Queue(object):
         self.last = new
         if self.first is None:
             self.first = self.last
+        self.size += 1
 
     def peek(self):
         return self.first.data
 
     def isEmpty(self):
-        return self.first is None
+        return self.size <= 0
 
 class Node(object):
     def __init__(self, val):
