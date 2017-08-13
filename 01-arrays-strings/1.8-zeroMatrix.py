93a969f330061535bc873a788b8b32f762026228
Harrison Lingren <harrisonlingren@gmail.com>
2017-08-12 18:28:51 -0700

    slight optimization

--- 01-arrays-strings/1.8-zeroMatrix.py
+++ 01-arrays-strings/1.8-zeroMatrix.py
@@ -3,7 +3,8 @@ def zeroMatrix(matrix):
     for i in range(len(matrix)):
         for j in matrix[i]:
             if j == 0:
-                target.append(i)
+                if i not in target:
+                    target.append(i)
 
     for row in target:
         size = len(matrix[row])
@@ -13,7 +14,7 @@ def zeroMatrix(matrix):
 _m = [
     [1,2,3,4,5,6,7,8],
     [4,5,7,6,8,4,2,5],
-    [1,2,6,4,7,0,7,4],
+    [1,2,0,0,7,0,7,4],
     [4,5,7,2,5,8,9,3],
     [4,0,4,5,7,8,2,3]
 ]
