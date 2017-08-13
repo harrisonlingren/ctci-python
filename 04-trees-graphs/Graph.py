2dbda970e5fab6bea0a9aadf88f5983b39b7f469
Harrison Lingren <harrisonlingren@gmail.com>
2017-08-12 22:24:50 -0700

    fixed Graph issues

--- 04-trees-graphs/Graph.py
+++ 04-trees-graphs/Graph.py
@@ -1,9 +1,17 @@
 class Graph(object):
-    def __init__(self):
-        self.nodes = []
-        self.root = Node()
+    def __init__(self, size=1):
+        self.nodes = [Node(x) for x in range(size)]
 
 class Node(object):
     def __init__(self, val=None):
         self.data = val
-        self.nodes = []
+        self.adjacent = []
+        self.visited = False
+
+    def __str__(self):
+        return str(self.data)
+    def __repr__(self):
+        return str(self.data)
+
+    def addAdj(self, noderef):
+        self.adjacent.append(noderef)
